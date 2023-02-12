356cc6e4774d7e1473a339c58602000ace76aa63
nrylord <neoforest3@gmail.com>
2023-02-12 17:36:33 +0300

    added axios interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/assets/styles/colors.sass'
-import UI from './components/ui'
+import '@/assets/variables/_colors.sass'
 import {createPinia} from "pinia";
+import axios from "axios";
+import {useCharacterStore} from "@/store";
 
 const pinia = createPinia()
 
+
+
 const app = createApp(App)
-
-UI.forEach((component) => {
-    app.component(component.name, component.component)
-})
-
 
 app
     .use(router)
     .use(pinia)
     .mount('#app')
+
+
+const store = useCharacterStore()
+axios.interceptors.response.use(
+    (res) => res,
+    (error) => {
+        if (error.response.status === 404) {
+            store.setErrorMessage('По заданным параметрам ничего не найдено')
+        } else {
+            throw error
+        }
+    }
+);
